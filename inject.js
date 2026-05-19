9b2dcfd72e726032a16f8b8cb80837a086daaa27
bibica <119997727+bibicadotnet@users.noreply.github.com>
2026-05-19 13:15:09 +0700

    Add files via upload

--- inject.js
+++ inject.js
@@ -316,6 +316,7 @@
             if (action === 'BLOCK') return;
             if (action === 'ASK') { askPopup(this.action, this.target || '_blank', ''); return; }
         }
+        if (!this.isConnected) return;
         return originalSubmit.call(this);
     };
 
